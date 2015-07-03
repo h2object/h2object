619e3fb93a06f032eb54f83ed4e715410b459a6c
刘建平 <liujianping@h2object.io>
2015-07-03 21:15:13 +0800

    add fmt for windows

--- commands/daemon_windows.go
+++ commands/daemon_windows.go
@@ -1,12 +1,11 @@
 package commands
 
 import (
+	"fmt"
 	"github.com/h2object/h2object/app"
 )
 
 func daemonize(application *app.Application) {
-	if mode {
-		fmt.Println("[h2object] warn: ", "windows not support daemon mode")
-	}
+	fmt.Println("[h2object] warn: ", "windows not support daemon mode")
 	run(application)	
 }
